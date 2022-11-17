78df437a074a26d38c4c5e61f9d6819aa4096a6c
Umutcan Karakaş <umutcankarakas@gmail.com>
2022-11-17 18:42:41 +0300

    Comments added

--- ReceiptScanParser/Helper/ParserHelper.cs
+++ ReceiptScanParser/Helper/ParserHelper.cs
@@ -10,6 +10,7 @@ namespace ReceiptScanParser.Helper
 {
     public class ParserHelper
     {
+        //Loading the json file and converting it to the list of models
         public static List<ResponseModel> LoadJson()
         {
             string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Data\\response.json";
@@ -21,43 +22,60 @@ namespace ReceiptScanParser.Helper
             }
 
         }
+        //Gets list of models as input and gives the list of final strings as output
         public static List<String> ParseList(List<ResponseModel> responseList)
         {
             List<String> output = new List<string>();
             List<ResponseModel> tempList = new List<ResponseModel>();
             String tempStr = "";
+
+            //First we calculate the upper and lower bounds of our line in the receipt by calculating the average y value
             double upperBound = (responseList[0].boundingPoly.vertices[0].y + responseList[0].boundingPoly.vertices[1].y)/2;
             double lowerBound = (responseList[0].boundingPoly.vertices[2].y + responseList[0].boundingPoly.vertices[3].y)/2;
             foreach (ResponseModel item in responseList)
             {
+                //Then we calculate the y value of the middle point of the box, surrounding a certain text
                 double midPoint = (item.boundingPoly.vertices[0].y + item.boundingPoly.vertices[1].y + +item.boundingPoly.vertices[2].y + +item.boundingPoly.vertices[3].y) / 4;
+                
+                //If that middle point is in between the bounds we calculated earlier, that means we are still on the same line
                 if(midPoint > upperBound && midPoint < lowerBound)
                 {
-                    tempList.Add(item);
+                    tempList.Add(item); //Since the orders of the texts in the same line could be mixed, so we are going to order them too
                 }
+                //Else, that means we are on the next line
                 else
                 {
+                    //Ordering the items -which are on the same line- by x values
                     tempList = tempList.OrderBy(o => o.boundingPoly.vertices[0].x).ToList();
+                    //Creating the string
                     foreach(var i in tempList)
                     {
                         tempStr += i.description + " ";
                     }
                     tempStr = tempStr.Remove(tempStr.Length - 1);
+
+                    //Adding this newly created string to the output
                     output.Add(tempStr);
+                    //Calculating borders of the next line
                     upperBound = (item.boundingPoly.vertices[0].y + item.boundingPoly.vertices[1].y) / 2;
                     lowerBound = (item.boundingPoly.vertices[2].y + item.boundingPoly.vertices[3].y) / 2;
+                    //Clearing the temp variables
                     tempStr = "";
                     tempList.Clear();
+                    //Adding the first item of the next line to the temp list and going back again
                     tempList.Add(item);
 
                 }
             }
+            //Handling the last line
             foreach (var i in tempList)
             {
                 tempStr += i.description + " ";
             }
             tempStr = tempStr.Remove(tempStr.Length - 1);
             output.Add(tempStr);
+
+            //Returning the final list of strings
             return output;
         }
     }
--- ReceiptScanParser/Program.cs
+++ ReceiptScanParser/Program.cs
@@ -11,11 +11,19 @@ namespace ReceiptScanParser
     {
         static void Main(string[] args)
         {
+            //Loading the json file and converting it to the list of models
             List<ResponseModel> ModelList = ParserHelper.LoadJson();
+
+            //The first item in the list is like a summary of the receipt, so we got rid of it
             ModelList = ModelList.Skip(1).ToList();
+
+            //The list generally sorted but some items are mixed so we are going to order them by y value of top left corner
             List<ResponseModel> SortedList = ModelList.OrderBy(o => o.boundingPoly.vertices[0].y).ToList();
+
+            //We get the final output, listed by line by line
             List<String> ParsedList = ParserHelper.ParseList(SortedList);
 
+            //Wrote the output to a text file
             using (TextWriter tw = new StreamWriter("Output.txt"))
             {
                 foreach (String s in ParsedList)
